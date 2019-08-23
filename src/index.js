6b547f92653fee01263e0108ea8dff63e94b7c9f
cheungww <1329105041@qq.com>
2019-08-24 00:12:34 +0800

    完成商品管理和品类管理

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+import App from './App.js';
+import memoryUtils from './utils/memoryUtils';
+import storageUtils from './utils/storageUtils';
+
+// 取出local中保存的 user数据，并存储到 memoryUtils；
+const user = storageUtils.getUser();
+memoryUtils.user = user;
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
---
+++ src/pages/role/role.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+// 用户路由
+class Role extends Component {
+
+  render () {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default Role;
