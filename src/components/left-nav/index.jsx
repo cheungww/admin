573b4e5dc37e71a2de43eea12264fe83f3d570a1
cheungww <1329105041@qq.com>
2019-08-29 00:48:56 +0800

    已完成角色管理和用户管理

--- src/components/left-nav/index.jsx
+++ src/components/left-nav/index.jsx
@@ -175,7 +175,7 @@ class LeftNav extends Component {
       <div className="left-nav">
         <Link to='/' className="left-nav-header">
           <img src={logo} alt="logo"/>
-          <h1>硅谷后台</h1>
+          <h1>电商后台</h1>
         </Link>
 
         <Menu
--- src/pages/admin/admin.jsx
+++ src/pages/admin/admin.jsx
@@ -8,8 +8,8 @@ import Header from '../../components/header'
 import Home from '../home/home'
 import Category from '../category/category'
 import Product from '../product/product'
-// import Role from '../role/role'
-// import User from '../user/user'
+import Role from '../role/role'
+import User from '../user/user'
 // import Bar from '../charts/bar'
 // import Line from '../charts/line'
 // import Pie from '../charts/pie'
@@ -43,9 +43,9 @@ export default class Admin extends Component {
               <Route path='/home' component={Home}/>
               <Route path='/category' component={Category}/>
               <Route path='/product' component={Product}/>
-              {/* <Route path='/user' component={User}/>
+              <Route path='/user' component={User}/>
               <Route path='/role' component={Role}/>
-              <Route path="/charts/bar" component={Bar}/>
+              {/* <Route path="/charts/bar" component={Bar}/>
               <Route path="/charts/pie" component={Pie}/>
               <Route path="/charts/line" component={Line}/>
               <Route path="/order" component={Order}/>
