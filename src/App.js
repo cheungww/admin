c5a19aa9a721edcad841a9dadc95603b5bc3f3a6
cheungww <1329105041@qq.com>
2019-10-06 23:49:35 +0800

    更改前端路由

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import {HashRouter, Route, Switch} from 'react-router-dom'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
 
 import Login from './pages/login/login'
 import Admin from './pages/admin/admin'
@@ -12,12 +12,12 @@ export default class App extends Component {
 
   render () {
     return (
-      <HashRouter>
+      <BrowserRouter>
         <Switch> {/*只匹配其中一个*/}
           <Route path='/login' component={Login}></Route>
           <Route path='/' component={Admin}></Route>
         </Switch>
-      </HashRouter>
+      </BrowserRouter>
     )
   }
 }
